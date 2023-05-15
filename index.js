07b2f4d9cd48a8ac8b2fe1757106b71a2f5a4806
taahirahismail <taahirah.i@gmail.com>
2023-05-15 15:25:21 +0200

    idk

---
+++ index.js
@@ -0,0 +1,29 @@
+// From the first task thingy (lecture 1.4 or something)
+console.log('Hello World');
+
+// variables and constants
+let n = 5;
+console.log(n);
+
+let z = 'poop';
+console.log(z);
+
+const interestRate = 0.3;
+console.log(interestRate);
+
+// that other thing Oslin said to do suma one time
+const person = {name: 'Timothy', surname: 'Barry', occupation: 'Idiot', age: 12, gender: 'Ask Jesus'}
+let {name, surname, occupation, age, gender} = person;
+console.log(name);
+console.log(surname);
+console.log(occupation);
+console.log(age);
+console.log(gender);
+
+let a, b, c; a=b=c=0;
+c--;
+document.write("the value of a is :", a + "<br/>");
+document.write("the value of a is :", b + "<br/>");
+document.write("the value of a is :", c + "<br/>");
+
+// data types
